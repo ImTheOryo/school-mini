20319321a50ad612868d665ad47928f2aa083bf9
ImTheOryo <oryo@Mac.lan>
2025-11-12 21:31:45 +0100

    Feat: class Student

---
+++ src/student.ts
@@ -0,0 +1,22 @@
+import {Person} from "./person";
+
+class Student extends Person {
+    private _grades: number[];
+
+    constructor(firstName: string, lastName: string) {
+        super(firstName, lastName);
+        this._grades = [];
+    }
+
+    getAverageGrade() {
+        if (this._grades.length === 0) return 0;
+
+        const sum = this._grades.reduce((sum, grade) => sum + grade, 0);
+        return parseFloat((sum / this._grades.length).toFixed(2));
+    }
+
+    addGrade(grade: number) {
+        if (grade < 0) return 0;
+        this._grades.push(grade);
+    }
+}
